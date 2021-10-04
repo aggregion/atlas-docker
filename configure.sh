38cb5bce8eb31fbe6e38181e97259883135db1fd
Denis Bezrukov <anthrax63@gmail.com>
2021-10-04 13:13:12 +0300

    update

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@ TEMPLATE_FILE=${CONF_DIR}/atlas-application.properties.template
 PROPS_FILE=${CONF_DIR}/atlas-application.properties
 
 export CASSANDRA_HOST=${CASSANDRA_HOST:=cassandra}
-export export CASSANDRA_PORT=${CASSANDRA_PORT:=9160}
+export CASSANDRA_PORT=${CASSANDRA_PORT:=9160}
 export ZOOKEEPER_HOST=${ZOOKEEPER_HOST:=zoo1,zoo2,zoo3}
 export SOLR_HOST=${SOLR_HOST:=solr}
 export SOLR_PORT=${SOLR_PORT:=8983}
