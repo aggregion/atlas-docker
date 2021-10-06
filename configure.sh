9e13780a52ce2b6b3d27097deff7021777918c3d
zbitname <i@zbit.name>
2021-10-06 11:53:04 +0300

    User should be better :)

--- configure.sh
+++ configure.sh
@@ -13,10 +13,11 @@ export KAFKA_HOST=${KAFKA_HOST:=kafka}
 
 envsubst < ${TEMPLATE_FILE} > ${PROPS_FILE}
 
-encryptedPwd=$(${ATLAS_HOME}/bin/cputil.py -g -u "${ATLAS_USER_ADMIN_LOGIN}" -p "${ATLAS_USER_ADMIN_PASSWORD}" -s)
-encryptedPwdDST=$(${ATLAS_HOME}/bin/cputil.py -g -u "${ATLAS_USER_DST_LOGIN}" -p "${ATLAS_USER_DST_PASSWORD}" -s)
-encryptedPwdDSCI=$(${ATLAS_HOME}/bin/cputil.py -g -u "${ATLAS_USER_DSCI_LOGIN}" -p "${ATLAS_USER_DSCI_PASSWORD}" -s)
+ln -s /usr/bin/python2.7 /usr/bin/python
+encryptedPwd=$(${ATLAS_HOME}/bin/cputil.py -g -u ${ATLAS_USER_ADMIN_LOGIN} -p ${ATLAS_USER_ADMIN_PASSWORD} -s)
+encryptedPwdDST=$(${ATLAS_HOME}/bin/cputil.py -g -u ${ATLAS_USER_DST_LOGIN} -p ${ATLAS_USER_DST_PASSWORD} -s)
+encryptedPwdDSCI=$(${ATLAS_HOME}/bin/cputil.py -g -u ${ATLAS_USER_DSCI_LOGIN} -p ${ATLAS_USER_DSCI_PASSWORD} -s)
 
-echo "admin=ADMIN::${encryptedPwd}" > ${ATLAS_HOME}/conf/users-credentials.properties
-echo "datasteward=DATA_STEWARD::${encryptedPwdDST}" >> ${ATLAS_HOME}/conf/users-credentials.properties
-echo "datascientist=DATA_SCIENTIST::${encryptedPwdDSCI}" >> ${ATLAS_HOME}/conf/users-credentials.properties
+echo "${ATLAS_USER_ADMIN_LOGIN}=ADMIN::${encryptedPwd}" > ${ATLAS_HOME}/conf/users-credentials.properties
+echo "${ATLAS_USER_DST_LOGIN}=DATA_STEWARD::${encryptedPwdDST}" >> ${ATLAS_HOME}/conf/users-credentials.properties
+echo "${ATLAS_USER_DSCI_LOGIN}=DATA_SCIENTIST::${encryptedPwdDSCI}" >> ${ATLAS_HOME}/conf/users-credentials.properties
