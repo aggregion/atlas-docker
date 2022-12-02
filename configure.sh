1d403267473e25bca84fdb843fb365826c485e7a
Dmitry Somov <smv.dmitry@gmail.com>
2022-12-02 17:12:15 +0300

    feat: support keycloak auth (DCP-2244)

--- configure.sh
+++ configure.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-CONF_DIR=${ATLAS_HOME:=/opt/atlas}/conf
+CONF_DIR=/Users/dmitrysomov/projects/aggregion/backend/atlas-docker
 TEMPLATE_FILE=${CONF_DIR}/atlas-application.properties.template
 PROPS_FILE=${CONF_DIR}/atlas-application.properties
+KEYLOAK_JSON_FILE=${CONF_DIR}/keycloak.json
 
 export CASSANDRA_HOST=${CASSANDRA_HOST:=cassandra}
 export CASSANDRA_PORT=${CASSANDRA_PORT:=9042}
@@ -11,6 +12,38 @@ export SOLR_HOST=${SOLR_HOST:=localhost}
 export SOLR_PORT=${SOLR_PORT:=8983}
 export KAFKA_HOST=${KAFKA_HOST:=kafka}
 export SOLR_ZOOKEEPER_HOST=${SOLR_ZOOKEEPER_HOST:=$ZOOKEEPER_HOST/solr}
+
+export FILE_ENABLE_AUTH=${FILE_ENABLE_AUTH:=true}
+
+export KEYCLOAK_ENABLE_AUTH=${KEYCLOAK_ENABLE_AUTH:=false}
+export KEYCLOAK_REALM=${KEYCLOAK_REALM:=aggregion}
+export KEYCLOAK_RESOURCE=${KEYCLOAK_RESOURCE:=atlas} # client id
+export KEYCLOAK_CREDENTIALS_SECRET=${KEYCLOAK_CREDENTIALS_SECRET:=atlasclientsecret}
+export KEYCLOAK_SSL_REQUIRED=${KEYCLOAK_SSL_REQUIRED:=external}
+export KEYCLOAK_AUTH_SERVER_URL=${KEYCLOAK_AUTH_SERVER_URL:=https://keycloak.aggregion.com/}
+# you can get keycloak.json from Keycloak Admin Console:
+# Clients -> Client Details -> Action -> Download adapter config -> Format option: "Keycloak OIDC JSON"
+export KEYCLOAK_JSON="
+{
+  \"realm\": \"$KEYCLOAK_REALM\",
+  \"auth-server-url\": \"$KEYCLOAK_AUTH_SERVER_URL\",
+  \"ssl-required\": \"$KEYCLOAK_SSL_REQUIRED\",
+  \"resource\": \"$KEYCLOAK_RESOURCE\",
+  \"credentials\": {
+    \"secret\": \"$KEYCLOAK_CREDENTIALS_SECRET\"
+  },
+  \"confidential-port\": 0
+}
+"
+
+if [ "$KEYCLOAK_ENABLE_AUTH" = "true" ]; then
+  echo "$KEYCLOAK_JSON" > $KEYLOAK_JSON_FILE
+  export KEYCLOAK_AUTH="
+######## Keycloak properties #########
+atlas.authentication.method.keycloak=true
+atlas.authentication.method.keycloak.file=keycloak.json
+"
+fi
 
 envsubst < ${TEMPLATE_FILE} > ${PROPS_FILE}
 
